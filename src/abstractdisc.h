55155fed6bfd9454b1582bb63bc37540eb43fe35
pashazz <pzinin@gmail.com>
2010-07-17 17:57:17 +0400

    AbstractDisc: initial implementation

---
+++ src/abstractdisc.h
@@ -0,0 +1,35 @@
+/*
+    Winegame - small utility to prepare Wine and install win32 apps in it.
+    Copyright (C) 2010 Pavel Zinin <pzinin@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+
+#ifndef ABSTRACTDISC_H
+#define ABSTRACTDISC_H
+#include <QtCore>
+
+class AbstractDisc
+{
+public:
+	AbstractDisc(QString path) {_path = path;}
+	bool mount () = 0;
+	bool umount () = 0;
+protected:
+	QString _path;
+
+};
+
+#endif // ABSTRACTDISC_H
